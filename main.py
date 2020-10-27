da80a3a692deca964534e0b07582af7228fa0996
SangMokChae <forhs13@naver.com>
2020-10-27 22:42:55 +0900

    yesterdayspython

---
+++ main.py
@@ -0,0 +1,26 @@
+# def p_plus(a, b):
+#   return a + b
+
+# def r_plus(a, b):
+#   return a + b
+
+# p_result = p_plus(10, 5)
+# r_result = r_plus(20, 10)
+
+
+# print(p_result, r_result)
+
+def r_plus(a, b):
+  return a + b
+  print("sometning llfdlskafjdoasifdujapofiudaspofudafjd", True)
+
+r_result = r_plus(2, 4)
+
+print(r_result)
+
+def p_plus(a, b):
+  print(a + b)
+
+p_result = p_plus(2, 4)
+
+print(p_result)
