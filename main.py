9434f2ccf7dbd87014f35323ab112dbd9627d1a8
SangMokChae <forhs13@naver.com>
2020-10-28 00:19:11 +0900

    hmm.. i will find

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def plus(a, b):
-  return int(a) + int(b)
+  return int(a) + len(b)
 
 def minus(a, b):
   return int(a) - int(b)
@@ -19,18 +19,21 @@ def power(a, b):
 def reminder(a, b):
   return int(a) % int(b)
 
-r_plus = plus(3, 2)
-r_minus = minus(4, 2)
-r_times = times(3, 2)
-r_division = division(6, 2)
-r_negation = negation(5, 2)
-r_power = power(7, 2)
-r_reminder = reminder(5, 2)
+r_plus = plus(3, "hello")
+# r_minus = minus(4, 2)
+# r_times = times(3, 2)
+# r_division = division(6, 2)
+# r_negation = negation(5, 2)
+# r_power = power(7, 2)
+# r_reminder = reminder(5, 2)
 
 print(r_plus)
-print(r_minus)
-print(r_times)
-print(r_division)
-print(r_negation)
-print(r_power)
-print(r_reminder)
+# print(r_minus)
+# print(r_times)
+# print(r_division)
+# print(r_negation)
+# print(r_power)
+# print(r_reminder)
+
+r_plus = plus(3, 2)
+print(r_plus)
