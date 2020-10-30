71350df167fa350fcfa016770012d86142212870
SangMokChae <forhs13@naver.com>
2020-10-30 23:36:27 +0900

    learn modules

--- class.py
+++ class.py
@@ -1,23 +1,3 @@
-def plus(a, b):
-  if type(b) is str:
-    return a + len(b)
-  else:
-    return a + b
+from calculator import plus, minus
 
-print(plus(12, "hello"))
-
-def minus(a, b):
-  if type(b) is bool:
-    return None
-  else:
-    return a + b
-
-print(minus(12, False))
-
-def r_plus(a, b):
-  if type(b) is int or type(b) is float:
-    return a + b
-  else:
-    return a + len(b)
-
-print(r_plus(10, "python"))
+print(plus(1, 2), minus(1, 2), True, "lallalala", False)
